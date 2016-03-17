8bdbefda9bca1f43f6a2e837a236140b940d0e98
David <davnit31@gmail.com>
2016-03-17 08:03:37 -0400

    Added functionality to read and output info on a directory of log files.

--- SBLogAnalyzer/Program.cs
+++ SBLogAnalyzer/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SBLogAnalyzer.Data;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,58 @@ namespace SBLogAnalyzer
     {
         static void Main(string[] args)
         {
+            DirectoryInfo logDir = new DirectoryInfo(Path.Combine(Environment.CurrentDirectory, "Logs"));
+            if (!logDir.Exists)
+            {
+                Console.WriteLine("Log file directory doesn't exist.");
+                logDir.Create();
+            }
+            else
+            {
+                string[] doNotProcess = new string[4] { "WHISPERS", "PACKETLOG", "master", "commands" };
+
+                List<LogMessage> messages = new List<LogMessage>();
+
+                int fileCount = 0, totalLines = 0;
+                long totalSize = 0;
+                foreach (FileInfo file in logDir.EnumerateFiles("*.txt"))
+                {
+                    if (file.Name.ContainsAny(doNotProcess))
+                        continue;
+
+                    long fileSize = file.Length;
+                    totalSize += fileSize;
+                    Console.WriteLine("Processing file: {0} [{1} KB]", file.Name, (fileSize / 1000));
+
+                    int lineNumber = 0;
+
+                    fileCount++;
+                    StreamReader reader = file.OpenText();
+                    while (!reader.EndOfStream)
+                    {
+                        lineNumber++;
+                        totalLines++;
+
+                        string line = reader.ReadLine();
+                        if (line.Trim().Length > 0)
+                        {
+                            messages.Add(LogMessage.Parse(line));
+                        }
+                    }
+                    Console.WriteLine("\tRead {0} lines.", lineNumber);
+                }
+
+                Console.WriteLine("Processing complete.");
+                Console.WriteLine();
+
+                Console.WriteLine("  Messages: {0}", messages.Count);
+                Console.WriteLine("     Files: {0}", fileCount);
+                Console.WriteLine("     Lines: {0}", totalLines);
+                Console.WriteLine("Total Size: {0}", totalSize);
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ SBLogAnalyzer/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBLogAnalyzer
+{
+    public static class StringExtension
+    {
+        public static bool ContainsAny(this string str, string[] values)
+        {
+            foreach (string v in values)
+                if (str.Contains(v))
+                    return true;
+            return false;
+        }
+    }
+}
